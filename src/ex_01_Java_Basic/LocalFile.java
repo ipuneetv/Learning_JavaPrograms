e6a94e182034215220d6ed3a10be471aef11fc76
puneet <ipuneetv@gmail.com>
2025-04-15 08:48:06 +0530

    15-04-2025 : Labs Added

---
+++ src/ex_01_Java_Basic/LocalFile.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class LocalFile {
+    public static void main(String[] args) {
+        System.out.println("Final");
+    }
+
+    public static void main(String args) {
+        System.out.println("name");
+    }
+
+    public static void main(int args) {
+        System.out.println("2");
+    }
+}
