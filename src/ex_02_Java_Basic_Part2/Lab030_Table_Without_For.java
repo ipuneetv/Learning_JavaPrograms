464ee0562c32c753fa70ab6a9ad05556e4907cbc
PVQA <ipuneetv@gmail.com>
2025-07-22 08:16:54 +0530

    22-07-2025 : Updated Labs

---
+++ src/ex_02_Java_Basic_Part2/Lab030_Table_Without_For.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basic_Part2;
+
+public class Lab030_Table_Without_For {
+    public static void main(String[] args) {
+        int num =9;
+        System.out.println("Multiplication Table of "+num+":");
+        System.out.printf("%d*1=%d%n",num,(num));
+        System.out.printf("%d*2=%d%n",num,(num*2));
+        System.out.printf("%d*3=%d%n",num,(num*3));
+        System.out.printf("%d*4=%d%n",num,(num*4));
+        System.out.printf("%d*5=%d%n",num,(num*5));
+        System.out.printf("%d*6=%d%n",num,(num*6));
+        System.out.printf("%d*7=%d%n",num,(num*7));
+        System.out.printf("%d*8=%d%n",num,(num*8));
+        System.out.printf("%d*9=%d%n",num,(num*9));
+        System.out.printf("%d*10=%d%n",num,(num*10));
+
+    }
+}
--- src/ex_02_Java_Basic_Part2/Lab030_Tables_With_For.java
+++ src/ex_02_Java_Basic_Part2/Lab030_Tables_With_For.java
@@ -1,10 +1,10 @@
 package ex_02_Java_Basic_Part2;
 
-public class Lab030_Tables {
+public class Lab030_Tables_With_For {
     public static void main(String[] args) {
         int number = 9;
         System.out.println("Table of 9");
-        for (int i = 1 ; i <= 10 ; i++)
+        for (int i = 1 ; i <= 10 ; i++) // but we have to do our eercise without for loop
         {
             System.out.printf("%d * %d = %d\n", number, i, number *i);
         }
