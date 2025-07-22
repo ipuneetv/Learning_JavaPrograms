081c2af20b02a79a1933c85bdde39d41e9c37af7
PVQA <ipuneetv@gmail.com>
2025-07-22 08:23:02 +0530

    22-07-2025 : Updated Labs

--- src/ex_02_Java_Basic_Part2/Lab030_Table_Without_For.java
+++ src/ex_02_Java_Basic_Part2/Lab030_Table_Without_For.java
@@ -15,5 +15,18 @@ public class Lab030_Table_Without_For {
         System.out.printf("%d*9=%d%n",num,(num*9));
         System.out.printf("%d*10=%d%n",num,(num*10));
 
+
+        System.out.println("Multiplication Table of "+num+":");
+        System.out.printf("%d*1=%d\n",num,(num));
+        System.out.printf("%d*2=%d\n",num,(num*2));
+        System.out.printf("%d*3=%d\n",num,(num*3));
+        System.out.printf("%d*4=%d\n",num,(num*4));
+        System.out.printf("%d*5=%d\n",num,(num*5));
+        System.out.printf("%d*6=%d\n",num,(num*6));
+        System.out.printf("%d*7=%d\n",num,(num*7));
+        System.out.printf("%d*8=%d\n",num,(num*8));
+        System.out.printf("%d*9=%d\n",num,(num*9));
+        System.out.printf("%d*10=%d\n",num,(num*10));
+
     }
 }
