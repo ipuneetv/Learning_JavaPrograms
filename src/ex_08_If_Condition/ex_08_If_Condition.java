ec781cb5292a56c02ef493208e0801f7e4d241c8
PVQA <ipuneetv@gmail.com>
2025-07-24 10:20:40 +0530

    24-07-2025 : Updated Labs

---
+++ src/ex_08_If_Condition/ex_08_If_Condition.java
@@ -0,0 +1,10 @@
+package ex_08_If_Condition;
+
+public class ex_08_If_Condition {
+    public static void main(String[] args) {
+        int age= 17;
+        if(age > 18){
+            System.out.println("Yes you are allowed to vote!");
+        }
+    }
+}
