f6ef85291371b6500ae64c644d9e503e060df835
PVQA <ipuneetv@gmail.com>
2025-08-06 08:13:14 +0530

    06-08-2025 : Updated Labs

---
+++ src/ex_14_Strings/Lab135_String_Immutable.java
@@ -0,0 +1,9 @@
+package ex_14_Strings;
+
+public class Lab135_String_Immutable {
+    public static void main(String[] args) {
+        String name = "puneett";
+        name.toUpperCase(); // PRAMOD
+        System.out.println(name);
+    }
+}
