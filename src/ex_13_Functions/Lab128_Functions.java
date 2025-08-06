42aedf2922d8bc5dd4eb1c5e21a6a9a0b94d54d0
PVQA <ipuneetv@gmail.com>
2025-08-06 08:48:51 +0530

    06-08-2025 : Updated Labs

--- src/ex_13_Functions/Lab128_Functions.java
+++ src/ex_13_Functions/Lab128_Functions.java
@@ -5,13 +5,13 @@ public class Lab128_Functions {
     public static void main(String[] args) {
         // Built - in
         // Created by the java Guys.
-        int result = Math.max(3,4);
+        int max = Math.max(3,4); // type math. and you will find all math related functions
         int min = Math.min(3,4);
-        System.out.println(result);
-        System.out.println(min);
+        System.out.println("max is : " +  max);
+        System.out.println("min is : " +  min);
 
-        //
-
+        //In case of Built In function JVM is calling function
+        // we just call these functions, since these are already defines
 
 
 
